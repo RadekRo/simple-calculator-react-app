dbc403221de6c7730a01ce8415be338377d70f94
Radosław Rocławski <r.roclawski@ankara.pl>
2018-04-23 08:08:29 +0200

    Basic stylization for the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <div className='calculator'>
-          <div className='result'>Result frame</div>
+          <div className='result'>0</div>
           <div className='clear'>Clear</div>
-          <div className='button'>/</div>
+          <div className='button operator'>/</div>
           <div className='button'>7</div>
           <div className='button'>8</div>
           <div className='button'>9</div>
-          <div className='button'>-</div>
+          <div className='button operator'>-</div>
           <div className='button'>4</div>
           <div className='button'>5</div>
           <div className='button'>6</div>
-          <div className='button'>+</div>
+          <div className='button operator'>+</div>
           <div className='button'>1</div>
           <div className='button'>2</div>
           <div className='button'>3</div>
-          <div className='button'>=</div>
+          <div className='button operator'>=</div>
         </div>
       </React.Fragment>
     );
