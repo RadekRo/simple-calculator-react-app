d954ec185da05b35184a8650eb738c222aa1f7af
Radosław Rocławski <r.roclawski@ankara.pl>
2018-04-23 08:35:23 +0200

    Replaced divide symbol with proper html code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
         <div className='calculator'>
           <div className='result'>0</div>
           <div className='clear'>Clear</div>
-          <div className='button operator'>/</div>
+          <div className='button operator'>&divide;</div>
           <div className='button'>7</div>
           <div className='button'>8</div>
           <div className='button'>9</div>
