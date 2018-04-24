8c1a8b643a7da1461ff4bcdf4f2dddd50ab7faae
Radosław Rocławski <r.roclawski@ankara.pl>
2018-04-24 09:44:59 +0200

    Created all operators icons on click. Cleared after pressing clear

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ class App extends Component {
           numberExposed: 0,
           operatorActive: false,
 
-      })
+      });
+
+      document.getElementById('operator-display').setAttribute('style', 'display: none');
 
   };
 
@@ -51,22 +53,28 @@ class App extends Component {
       <React.Fragment>
         <div className='calculator'>
           <div className='result'>
-              <div id='operator-display'>&divide;</div>
+              <div id='operator-display'>{ this.state.operatorActive }</div>
               { this.state.numberExposed }
           </div>
           <div className='clear' onClick={ this.clearPressed }>clear</div>
           <div className='button operator'
                onClick={ this.operatorPressed }
-               data-key='divide'
+               data-key='&divide;'
           >&divide;</div>
           <div className='button' onClick={ this.numberPressed } data-key='7'>7</div>
           <div className='button' onClick={ this.numberPressed } data-key='8'>8</div>
           <div className='button' onClick={ this.numberPressed } data-key='9'>9</div>
-          <div className='button operator'>-</div>
+          <div className='button operator'
+                onClick={ this.operatorPressed }
+                data-key='-'
+          >-</div>
           <div className='button' onClick={ this.numberPressed } data-key='4'>4</div>
           <div className='button' onClick={ this.numberPressed } data-key='5'>5</div>
           <div className='button' onClick={ this.numberPressed } data-key='6'>6</div>
-          <div className='button operator'>+</div>
+          <div className='button operator'
+                onClick={ this.operatorPressed }
+                data-key='+'
+          >+</div>
           <div className='button' onClick={ this.numberPressed } data-key='1'>1</div>
           <div className='button' onClick={ this.numberPressed } data-key='2'>2</div>
           <div className='button' onClick={ this.numberPressed } data-key='3'>3</div>
